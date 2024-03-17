1b23795e29a28d7bfe8a793ad46ff21a658e2c50
KashfyZul <e1121648@u.nus.edu>
2024-03-17 15:35:47 +0700

    a* kinda works ig

--- maze.py
+++ maze.py
@@ -1,5 +1,8 @@
 import rclpy
 from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 from rclpy.qos import qos_profile_sensor_data
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
@@ -9,6 +12,16 @@ import scipy.stats
 from geometry_msgs.msg import TransformStamped
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 import cv2 as cv
+#from pyamaze import maze,agent,textLabel
+from queue import PriorityQueue
+import math
+import tf2_ros
+from tf2_ros import LookupException, ConnectivityException, ExtrapolationException
+import scipy.stats
+
+UNKNOWN = 1
+UNOCCUPIED = 2
+OCCUPIED = 3
 
 # constants
 occ_bins = [-1, 0, 50, 100]
@@ -37,22 +50,155 @@ def dilate123(src, size=1, shape=cv.MORPH_RECT):
     return np.maximum(src, array_dilated)
 
 
+
+# use this function to convert raw odata coordinates to reference the defined origin
+def reference_to_origin(raw_odata_coord):
+    return (
+        raw_odata_coord[0] - odata_origin[0],
+        raw_odata_coord[1] - odata_origin[1]
+    )
+#use this function to dereference odata coordinates from the defined origin
+def dereference_to_origin(ref_odata_coord):
+    return (
+        ref_odata_coord[0] + odata_origin[0],
+        ref_odata_coord[1] + odata_origin[1]
+    )
+
+
 class Occupy(Node):
     def __init__(self):
         super().__init__("occupy")
+        
         self.subscription = self.create_subscription(
             OccupancyGrid, "map", self.occ_callback, qos_profile_sensor_data
         )
         self.subscription  # prevent unused variable warning
         # occdata = np.array([])
+        self.tfBuffer = tf2_ros.Buffer()
+        self.tfListener = tf2_ros.TransformListener(self.tfBuffer, self)
 
     def occ_callback(self, msg):
+
+        # use this function to convert from rviz coordinates to odata coordinates
+        def convert_to_rviz(odata_coord):
+            rviz = (
+            odata_coord[0] * msg.info.resolution + msg.info.origin.position.x,
+            odata_coord[1] * msg.info.resolution + msg.info.origin.position.y,
+            )
+            return rviz
+        # use this function to convert from odata coordinates to rviz coordinates
+        def convert_to_odata(rviz):
+            odata_coord = (
+
+                round(float(rviz[0] - msg.info.origin.position.x) / msg.info.resolution), 
+                round(float(rviz[1] - msg.info.origin.position.y) / msg.info.resolution) 
+            )
+            return odata_coord
+
+        # the process for obtaining odata (the occupancy grid)
         occdata = np.array(msg.data)
         _, _, binnum = scipy.stats.binned_statistic(
             occdata, np.nan, statistic="count", bins=occ_bins
         )
         odata = np.uint8(binnum.reshape(msg.info.height, msg.info.width))
         odata = dilate123(odata, size=4)  # 4 here means 20cm
+        (odata_y, odata_x) = odata.shape
+        self.get_logger().info("maze dilated")
+
+        # obtain the rviz coordinates of the turtlebot
+        # usually requires the Occupy() node to spin more than once
+        try:
+            trans = self.tfBuffer.lookup_transform('map', 'base_link', rclpy.time.Time())
+        except (LookupException, ConnectivityException, ExtrapolationException) as e:
+            self.get_logger().info('No transformation found')
+            return
+        cur_pos = trans.transform.translation 
+        self.get_logger().info('Trans: %f, %f' % (cur_pos.x, cur_pos.y))
+
+        # confirm our defined origin coordinates in odata, obtained from FirstOccupancy()
+        self.get_logger().info('odata_origin: ' + str(odata_origin))
+
+        # rviz coordinates of turtlebot's current position
+        temp_cur_pos = (cur_pos.x, cur_pos.y) 
+        # convert turtlebot's rviz coordinates to odata coordintates to be reflected in the occupancy grid
+        curr_pos_odata = convert_to_odata(temp_cur_pos) 
+        self.get_logger().info('curr_pos_odata: ' + str(curr_pos_odata))
+
+        # current position of turtlebot in odata, with reference to origin 
+        curr_pos = reference_to_origin(curr_pos_odata)
+        # add a cost variable (x, y, cost)
+        curr_pos = (curr_pos[0], curr_pos[1], 0)
+        # curr_pos = ((curr_pos_odata[0] - odata_origin[0]), (curr_pos_odata[1] - odata_origin[1]), 0)
+        self.get_logger().info('Current' + str(curr_pos))
+
+        goal = (0, 0)
+        # iterate through odata to find the goal (highest y coordinate)
+        maxnum = 0
+        for row in range(odata_y):
+            for col in range(odata_x): 
+                if odata[row][col] == 2 and row + col > maxnum:
+                    minnum = row + col
+                    goal = (col, row)
+
+        
+        # find goal_pos, the goal relative to the origin coordinates
+        goal_pos = reference_to_origin(goal)
+        # update goal_pos with the cost from the start to the goal
+        goal_pos = (goal_pos[0], goal_pos[1], cost_to_goal(curr_pos, goal_pos))
+        # goal_pos = ((goal[0] - odata_origin[0]), (goal[1] - odata_origin[1]), 0)
+        self.get_logger().info('Goal' + str(goal_pos))
+
+
+        # EVERYTHING SHOULD NOW BE CONVERTED TO ODATA COORDINATES
+
+        start_pos = curr_pos
+        
+
+        # mark the curr_pos on the 2D array (need to print absolute coordinates, not with reference to defined origin)
+        odata[int(curr_pos[1] + odata_origin[1]), int(curr_pos[0] + odata_origin[0])] = 4
+
+        self.get_logger().info("finding path...")
+
+        # bool variables to check if the current position is within range of goal
+        range_dist = 12
+        is_within_goal_y = (curr_pos[1] < goal_pos[1] + range_dist and curr_pos[1] > goal_pos[1] - range_dist)
+        is_within_goal_x = (curr_pos[0] < goal_pos[0] + range_dist and curr_pos[0] > goal_pos[0] - range_dist)
+
+        loop_count = 0
+        # exit the loop when current position is in range of the goal
+        while not (is_within_goal_x and is_within_goal_y):
+            new_pos = a_star(curr_pos, start_pos, goal_pos, odata) # finds next point to travel to
+            self.get_logger().info(str(new_pos))
+            
+            # need to print absolute coordinates, not with reference to defined origin
+            map_pos = dereference_to_origin(new_pos)
+            # loop to visibly observe curr_pos in the occupancy grid
+            for i in range(1):
+                odata[int(map_pos[1]), int(map_pos[0])] = 4
+            
+            # create image from 2D array using PIL
+            if loop_count % 15 == 0: # print new map only every 3 movements
+                img = Image.fromarray(odata)
+                # show the image using grayscale map
+                plt.imshow(img, cmap="gray", origin="lower")
+                plt.draw_all()
+                # pause to make sure the plot gets created
+                plt.pause(0.00000000001)
+            self.get_logger().info("finding next")
+            
+
+            # this is just to make sure the loop exits and doesn't loop again once path found
+            # if new_pos == (0, 0, 0):
+            #     break
+            
+            # update curr_pos 
+            curr_pos = new_pos
+            # update bool status of whether or not curr_pos is in range of goal
+            is_within_goal_y = (curr_pos[1] < goal_pos[1] + range_dist and curr_pos[1] > goal_pos[1] - range_dist)
+            is_within_goal_x = (curr_pos[0] < goal_pos[0] + range_dist and curr_pos[0] > goal_pos[0] - range_dist)
+            loop_count += 1
+
+        self.get_logger().info("path found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
         # create image from 2D array using PIL
         img = Image.fromarray(odata)
@@ -60,7 +206,12 @@ class Occupy(Node):
         plt.imshow(img, cmap="gray", origin="lower")
         plt.draw_all()
         # pause to make sure the plot gets created
-        plt.pause(0.00000000001)
+        plt.pause(5.00000000001)
+        
+        # exit this node once path found
+        raise SystemExit
+        
+
 
 
 class FirstOccupy(Node):
@@ -89,10 +240,14 @@ class FirstOccupy(Node):
                     minnum = i + j
                     current_min = (i, j)
 
+        global odata_origin
+        odata_origin = current_min
+        
         current_min = (
             current_min[0] * msg.info.resolution + msg.info.origin.position.x,
             current_min[1] * msg.info.resolution + msg.info.origin.position.y,
         )
+
 
         self.get_logger().info("New Origin: " + str(current_min))
         # self.get_logger().info(str(msg.info.resolution))
@@ -115,6 +270,58 @@ class FirstOccupy(Node):
         self.tf_static_broadcaster.sendTransform(t)
 
 
+# this takes in a matrix and finds the coordinates of the point with the highest y coordinate
+def find_greatest_y(m):
+    for row in range(m, -1, -1):
+        for col in range(m[row]): 
+            if m[row][col] == 2:
+                return (col, row)
+    return (0, 0)    
+        
+
+# calculates euclidean distance from current position to the goal
+def cost_to_goal(pos, goal_pos):
+    dist_x = abs(goal_pos[0] - pos[0])
+    dist_y = abs(goal_pos[1] - pos[1])
+    return math.sqrt(dist_x**2 + dist_y**2)
+
+# curr_pos = (x-coordinate, y-coordinate, cost from start point)
+def a_star(curr_pos, start_pos, goal_pos, gridmap):
+    # possible directions
+    """
+    Get all possible 8-connectivity movements. Equivalent to get_movements_in_radius(1).
+    :return: list of movements with cost [(dx, dy, movement_cost)]
+    """
+    s2 = math.sqrt(2)
+    directions = [(1, 0, 1.0), #right
+            (0, 1, 1.0), #forward
+            (-1, 0, 1.0), #left
+            (0, -1, 1.0), #back
+            (1, 1, s2), 
+            (-1, 1, s2),
+            (-1, -1, s2),
+            (1, -1, s2)]
+    next_pos = curr_pos
+    min_cost = 0
+    for direction in directions: 
+        new_pos = (int(curr_pos[0] + direction[0]), int(curr_pos[1] + direction[1]), int(curr_pos[2] + direction[2]))
+        #for i in range(0, 2, 1):
+         #   new_pos[i] += direction[i]
+        
+        temp_pos = dereference_to_origin(new_pos)
+        if gridmap[temp_pos[1], temp_pos[0]] == 2:
+            calc_cost = cost_to_goal(new_pos, goal_pos) + new_pos[2]
+        
+        # if min_cost == 0:
+        #     min_cost = calc_cost
+            if not min_cost or calc_cost < min_cost:
+                min_cost = calc_cost
+                next_pos = new_pos
+        else: 
+            continue
+    return next_pos
+            
+        
 def main(args=None):
     rclpy.init(args=args)
 
@@ -126,7 +333,13 @@ def main(args=None):
     plt.ion()
     plt.show()
 
-    rclpy.spin(occupy)
+    try:
+        rclpy.spin(occupy)
+    except SystemExit:                 # <--- process the exception 
+        rclpy.logging.get_logger("Quitting").info('Done')
+
+  
+    
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
