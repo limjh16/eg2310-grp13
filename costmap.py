cd037ef5ec0b777b2f1508e4e5068de83d624f27
limjh16 <limjh16@gmail.com>
2024-04-15 16:27:36 +0800

    change inflation numbers

--- costmap.py
+++ costmap.py
@@ -24,8 +24,8 @@ class Costmap(Node):
         odata = inflate(
             occdata.reshape(msg.info.height, msg.info.width),
             dilate=int((0.243 / 2) // msg.info.resolution + 1),
-            inflation_radius=3,
-            inflation_step=20,
+            inflation_radius=8,
+            inflation_step=5,
             threshold=52,
             erode=6,
         )
